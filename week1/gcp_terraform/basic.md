7243462de6f9facced191cddc32c459979c3effb
Mazaya Yumna <mazayayumnawijaya@gmail.com>
2024-02-26 22:56:42 +0700

    commit of basic terraform

--- week1/gcp_terraform/basic.md
+++ week1/gcp_terraform/basic.md
@@ -3,3 +3,15 @@
 ## Setting GCP
 1) Make a new project -> Service Account
 2) FIll details -> Grant Role for Storage, BigQuery, and Compute Admin (later can edit in IAM)
+3) Permission to run: Click action -> Manage Keys -> Add key
+4) Save json credential to /keys folder, export its path to $GOOGLE_CREDENTIALS
+5) Run Terraform command below, you can see the bucket in the cloud storage
+
+## Basic Terraform command
+```
+terraform fmt
+terraform init
+terraform plan
+terraform apply
+terraform destroy
+```
---
+++ week1/gcp_terraform/main.tf
@@ -0,0 +1,28 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "5.6.0"
+    }
+  }
+}
+
+provider "google" {
+  project = "terraform-demo-415514"
+  region  = "us-central"
+}
+
+resource "google_storage_bucket" "demo-bucket" {
+  name          = "terraform-demo-415514-terra-bucket"
+  location      = "US"
+  force_destroy = true
+
+  lifecycle_rule {
+    condition {
+      age = 1
+    }
+    action {
+      type = "AbortIncompleteMultipartUpload"
+    }
+  }
+}
